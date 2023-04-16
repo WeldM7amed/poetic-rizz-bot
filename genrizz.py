80c248893e5225a7752b2fd3a58ad4c3803302f3
WeldM7amed <82772341+WeldM7amed@users.noreply.github.com>
2023-04-16 14:53:40 +0100

    Update genrizz.py

--- genrizz.py
+++ genrizz.py
@@ -8,6 +8,6 @@ count = 0
 for path in os.listdir(dir_path):
     if os.path.isfile(os.path.join(dir_path, path)):
         count += 1
-for i in range(count+1):
+for i in range(count):
     p = """cmd /c "ffmpeg -i rizz.mp4 -c:v libx264 -crf 20 -c:a aac -strict -2 -vf "subtitles=subs/"""+str(i)+""".srt:force_style='MarginV=100'" rizz/rizz"""+str(i)+""".mp4" """
     os.system(p)
